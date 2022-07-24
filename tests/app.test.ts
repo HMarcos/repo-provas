38df587ffc565763762dc651631ba3631164769e
Marcos Henrique <marcosmarcone48@gmail.com>
2022-07-24 04:14:07 -0300

    TEST: create test suite for check the login flow

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -6,6 +6,7 @@ import userFactory from "./factories/userFactory.js";
 
 beforeEach(async () => {
     await prismaClient.$executeRaw`TRUNCATE TABLE users CASCADE`;
+    await prismaClient.$executeRaw`TRUNCATE TABLE sessions`;
 });
 
 describe("Sign up test suite", () => {
@@ -38,6 +39,42 @@ describe("Sign up test suite", () => {
         expect(response.status).toBe(409);
     });
 });
+
+describe("Sign in test suite", () => {
+    it("Send a valid login - Expect status 200 and token", async () => {
+        const signUpData = userFactory.createSignUpInfo();
+        await supertest(app).post('/sign-up').send(signUpData);
+
+        const loginData = { email: signUpData.email, password: signUpData.password };
+        const response = await supertest(app).post('/sign-in').send(loginData);
+        const token = response.body.token;
+        expect(response.status).toBe(200);
+        expect(token).not.toBeNull();
+    });
+
+    it("Send a login without body - Error 422", async () => {
+        const loginData = {};
+        const response = await supertest(app).post('/sign-in').send(loginData);
+        expect(response.status).toBe(422);
+    });
+
+    it("Send a login with an unregistered email - Error 401", async () => {
+        const loginData = {email: "teste@teste.com", password: "teste"};
+        const response = await supertest(app).post('/sign-in').send(loginData);
+        expect(response.status).toBe(401);
+    });
+
+    it("Send a login with the incorrect password - Error 401", async () => {
+        const signUpData = userFactory.createSignUpInfo();
+        await supertest(app).post('/sign-up').send(signUpData);
+
+        const loginData = { email: signUpData.email, password: "testeTeste" };
+        const response = await supertest(app).post('/sign-in').send(loginData);
+        expect(response.status).toBe(401);
+    });
+
+});
+
 
 afterAll(async () => {
     await prismaClient.$disconnect();
